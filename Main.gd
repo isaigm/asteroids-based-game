a67518adcd8aca371ea42ecbcc5cd38288232746
isaigm <isaigm23@gmail.com>
2020-07-30 20:00:37 -0500

    update main

--- Main.gd
+++ Main.gd
@@ -1,8 +1,43 @@
 extends Node
 export (PackedScene) var Asteroid
 
+func loadGame():
+	var max_score = loadFile()
+	if max_score.length() == 0:
+		$HUD/MaxScore.text = "Max score: 0"
+	else:
+		$HUD/MaxScore.text = "Max score: " + max_score
+
+func saveGame():
+	var score = $HUD/Score.text
+	var last_score = loadFile()
+	if last_score.length() == 0:
+		saveFile(score)
+	else:
+		saveFile(str(max(int(score), int(last_score))))
+
+func saveFile(content):
+	var file = File.new()
+	file.open("user://max_score.dat", File.WRITE)
+	file.store_string(content)
+	file.close()
+
+func loadFile():
+	var file = File.new()
+	file.open("user://max_score.dat", File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return content
+
+func _notification(what):
+	if (what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST):
+		print ("You are quit!")
+		saveGame()
+		get_tree().quit() # default behavior
+	
 func _ready():
 	randomize()
+	loadGame()
 	$AsteroidTimer.start()
 
 func _on_AsteroidTimer_timeout():
@@ -24,11 +59,13 @@ func _on_AsteroidTimer_timeout():
 func _on_Player_hit():
 	get_tree().call_group("asteroids", "queue_free")
 	$AsteroidTimer.stop()
-	$HUD.update_score(0)
 	$HUD/Score.hide()
 	$HUD/Button.show()
 	$Player.disable()
-
+	saveGame()
+	loadGame()
+	$HUD.update_score(0)
+	
 func _on_HUD_start():
 	$Player.start($StartPos.position)
 	$AsteroidTimer.start()
